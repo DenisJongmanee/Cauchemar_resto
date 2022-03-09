4f9a533f082fe7d746f5b4f42c9665855447b4e2
DenisJongmanee <jongmanee.denis@gmai.com>
2022-03-09 17:54:21 +0100

    add note restau

--- src/components/Restau.js
+++ src/components/Restau.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 class Restaurant extends Component {
   state={
     restau:this.props.restaurant
@@ -11,8 +11,12 @@ class Restaurant extends Component {
       <View style={styles.restau}>
         <Image style={styles.image} source={restau.image}/>
         <View style={styles.text}>
-          <View >
+          <View>
             <Text style={styles.titre}>{restau.titre}</Text>
+          </View>
+          <View style={styles.note}>
+            <MaterialCommunityIcons name="star-outline" color={"red"} size={25}/>
+            <Text>{restau.note}/5</Text>
           </View>
           <View style={styles.description}>
             <Text>{restau.description}</Text>
@@ -44,6 +48,10 @@ const styles = StyleSheet.create({
   },
   description: {
     marginRight:170
+  },
+  note: {
+    flexDirection:'row',
+    alignItems: 'center'
   }
 })
 
--- src/navbar.js
+++ src/navbar.js
@@ -10,13 +10,62 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
 
   const restaurants = [
-    {id:1,image:require("../assets/restaurant1.png"),titre: "Le dégueustaurant", description: "Sent le poisson pas frais autant à l'extérieur que sur le personnel du restaurant. Poisson qui a été péché dans la Seine.",ville:"Paris"},
-    {id:2,image:require("../assets/restaurant2.png"),titre: "La cuisson à point", description: "Pizza cuite au feu de bois, comme le restaurant. Une incroyable prise de risque et de mal d'estomac à prévoir.",ville:"Strasbourg"},
-    {id:3,image:require("../assets/restaurant3.png"),titre: "Le gras Vie et...", description: "Burger avec gras implanté à la piqûres et changement de toiture à prévoir.",ville:"Marseille"},
-    {id:4,image:require("../assets/restaurant4.png"),titre: "Végan de toi", description: "Besoin de déserbant pour ce restaurant végan. Feuille de salade trop sèche.",ville:"Bordeaux"},
-    {id:5,image:require("../assets/restaurant5.png"),titre: "Le Plaine Air", description: "Manque de chauffage et les fenêtre cassée n'aide pas.", ville:"Lille"},
-    {id:6,image:require("../assets/restaurant6.png"),titre: "Un titanic V2", description: "Tueur en série devant le restaurant, accueil à revoir.", ville:"Lille"},
-    {id:7,image:require("../assets/restaurant7.png"),titre: "Le Plein Vol", description: "L'aviateur est incompétent tout comme la cuisine. Crash assuré.", ville:"Lille"}
+    {
+      id: 1,
+      image:require("../assets/restaurant1.png"),
+      titre: "Le dégueustaurant", 
+      description: "Sent le poisson pas frais autant à l'extérieur que sur le personnel du restaurant. Poisson qui a été péché dans la Seine.",
+      ville:"Paris",
+      note: 0.2
+    },
+    {
+      id: 2,
+      image:require("../assets/restaurant2.png"),
+      titre: "La cuisson à point",
+      description: "Pizza cuite au feu de bois, comme le restaurant. Une incroyable prise de risque et de mal d'estomac à prévoir.",
+      ville:"Strasbourg",
+      note: 0.5
+    },
+    {
+      id: 3,
+      image:require("../assets/restaurant3.png"),
+      titre: "Le gras Vie et...",
+      description: "Burger avec gras implanté à la piqûres et changement de toiture à prévoir.",
+      ville:"Marseille",
+      note: 0.5
+    },
+    {
+      id: 4,
+      image:require("../assets/restaurant4.png"),
+      titre: "Végan de toi",
+      description: "Besoin de déserbant pour ce restaurant végan. Feuille de salade trop sèche.",
+      ville:"Bordeaux",
+      note: 0.7
+    },
+    {
+      id: 5,
+      image:require("../assets/restaurant5.png"),
+      titre: "Le Plaine Air", 
+      description: "Manque de chauffage et les fenêtre cassée n'aide pas.",
+      ville:"Lille",
+      note: 0.6
+    },
+    {
+      id: 6,
+      image:require("../assets/restaurant6.png"),
+      titre: "Un titanic V2",
+      description: "Tueur en série devant le restaurant, accueil à revoir.",
+      ville:"Lille",
+      note: 1
+    },
+    {
+      id: 7,
+      image:require("../assets/restaurant7.png"),
+      titre: "Le Plein Vol",
+      description: "L'aviateur est incompétent tout comme la cuisine. Crash assuré.",
+      ville:"Lille",
+      note: 0.8
+    }
   ]
 
   return (
