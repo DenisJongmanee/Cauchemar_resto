c7fa98e46fe4602f8fe3b04acf14459d29171711
thibautringeval <78933047+ThibautRingeval@users.noreply.github.com>
2022-03-09 10:47:40 +0100

    Restaurant

--- src/navbar.js
+++ src/navbar.js
@@ -11,9 +11,11 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
 
   const restaurants = [
-    {id:1,image:require("../assets/restaurant1.png"),titre: "Bonjour", description: "description"},
-    {id:2,image:require("../assets/restaurant1.png"),titre: "Bonjour", description: "description"},
-    {id:3,image:require("../assets/restaurant1.png"),titre: "Bonjour", description: "description"}
+    {id:1,image:require("../assets/restaurant1.png"),titre: "Bonjour", description: "Sent le poisson pas frais autant à l'extérieur que sur le le personnel du restaurant",ville:"Lille"},
+    {id:2,image:require("../assets/restaurant2.png"),titre: "Bonjour", description: "Pizza cuite au feu de bois, comme le restaurant. Une incroyable prise de risque et de mal d'estomac à prévoir",ville:"Lille"},
+    {id:3,image:require("../assets/restaurant3.png"),titre: "Bonjour", description: "Burger avec gras implanté à la piqûres et changement de toiture à prévoir",ville:"Lille"},
+    {id:4,image:require("../assets/restaurant4.png"),titre: "Bonjour", description: "Besoin de déserbant pour ce restaurant végan. Feuille de salade trop sèche.",ville:"Lille"},
+    {id:5,image:require("../assets/restaurant5.png"),titre: "Bonjour", description: "Manque de chauffage et les fenêtre cassée n'aide pas.", ville:"Lille"}
   ]
 
   return (
